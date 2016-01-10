2c0e3bbdd929569f57829a3c12295d9e736d1de9
Lauro Lins <lauro.lins@gmail.com>
2016-01-10 15:58:26 -0500

    towards the paper figure with the four variables of the ranking-merge problems

--- data/problems.R
+++ data/problems.R
@@ -1,6 +1,9 @@
 setwd("/Users/llins/projects/topkube_benchmark/data")
 t <- read.table("topkube_benchmark_problems_stats.psv",header=T,as.is=T,sep="|")
 
+rename = c("twitter", "github", "flickr", "wikipedia")
+names(rename) = c("tweets_40M","github_58M","flickr_84M","wikipedia_120M")
+
 density.all = sort(t$density)
 n = length(density.all)
 
@@ -13,7 +16,8 @@ pretty.name = function(x) {
   return(sapply(x,function(x) {
     if (x == 0) return("0")
     e = log(x,10)
-    if (e <= 0) { return(sprintf("%.1f",x)) }
+    if (e < -1) { return(sprintf("%.3f",x)) }
+    else if (e <= 0) { return(sprintf("%.1f",x)) }
     else if (e < 3) { return(sprintf("%.0f",x)) }
     else if (e < 4) { return(sprintf("%.0fk",x/1e3)) }
     else if (e < 5) { return(sprintf("%.0fk",x/1e3)) }
@@ -48,25 +52,38 @@ add.alpha <- function(col, alpha=1){
 # }
 
 plot.data = function(xinfo, yinfo, title, values, classes) {
-  par(mar=c(6,6,1,1))
-  plot(0,type="n",xlim=xinfo$lim,ylim=yinfo$lim,xlab=xinfo$label,ylab=yinfo$label,axes=F)
+
+  values.by.dataset = split(values, classes)
+  
+  line.width = c(4,2.5,2.5,2.5,2.5)
+  line.type  = c(1,3,2,4,5)
+  
+  par(mar=c(6,3,0.5,0.5))
+  # yinfo$label
+  plot(0,type="n",xlim=xinfo$lim,ylim=yinfo$lim,xlab=xinfo$label,ylab="",axes=F)
   axis(1,at=xinfo$ticks,labels=pretty.name(xinfo$ticks))
   axis(2,at=yinfo$ticks,labels=pretty.name(yinfo$ticks),las=2)
-  abline(h=yinfo$ticks,lwd=2,col=gray(0.8),lty=2)
-  abline(v=xinfo$ticks,lwd=2,col=gray(0.8),lty=2)
+  abline(h=yinfo$ticks,lwd=1,col=gray(0.8),lty=1)
+  abline(v=xinfo$ticks,lwd=1,col=gray(0.8),lty=1)
+
   
-  lines(values, cum.ratio(values), type="l",lwd=6, col=colors[1]) # overall density vs. population
-  values.by.dataset = split(values, classes)
+  legend.names  = c("all", sapply(names(values.by.dataset),function(n) rename[[n]]))
+  legend.lty    = 1:5
+  legend(xinfo$lim[2],0,legend.names,xjust=1,yjust=0,col=colors,lty=line.type,lwd=line.width,box.lwd=0,box.col="white",bg="#ffffffaa")
+  
+
+  print(title)
+  print(quantile(values,probs=seq(0,1,0.1)))
+  
+  lines(values, cum.ratio(values), type="l",lwd=line.width[1], lty=line.type[1], col=colors[1]) # overall density vs. population
   for (i in 1:length(values.by.dataset)) {
     x = values.by.dataset[[i]]
-    lines(x, cum.ratio(x), type="l",lwd=4,col=colors[1+i],lty=1+i) # overall density vs. population
+    lines(x, cum.ratio(x), type="l",lwd=line.width[i+1],col=colors[1+i],lty=line.type[1+i]) # overall density vs. population
+
+    print(names(values.by.dataset)[i])
+    print(quantile(x,probs=seq(0,1,0.1)))
   }
 
-  legend.names  = c("all", names(density.by.dataset))
-  legend.lty    = 1:5
-  legend.lwd    = c(6,4,4,4,4)
-  legend(xinfo$lim[2],0,legend.names,xjust=1,yjust=0,col=colors,lty=legend.lty,lwd=legend.lwd)
-  
 }
 
 
@@ -84,22 +101,30 @@ input = lapply(input.columns,
 names(input) = input.columns
 
 
-plot.new()
-columns = length(input)
 
-rng = c(0,0.8)
-
-for (col in 1:columns) {
-  # left plot
-  data = input[[col]]
-  v = data$values;
-  n = length(v)
-  # find value where
-  a = v[1 + as.integer((n-1)*rng[1])]
-  b = v[1 + as.integer((n-1)*rng[2])]
-  xinfo = axis.info(pretty(c(a,b),6),c(a,b),sprintf("%s",names(input)[col]))
-  yinfo = axis.info(seq(0,1,0.1),c(0,1),"cumulative problem count ratio")
-  par(fig=c((col-1)/columns,col/columns,0,1), new=TRUE)
-  plot.data(xinfo, yinfo, data$name, data$values, data$classes)
+render = function(filename, rng) {
+  pdf(filename,width=14,height=4,pointsize=10)
+  plot.new()
+  columns = length(input)
+  for (col in 1:columns) {
+    # left plot
+    data = input[[col]]
+    v = data$values;
+    n = length(v)
+    # find value where
+    a = v[1 + as.integer((n-1)*rng[1])]
+    b = v[1 + as.integer((n-1)*rng[2])]
+    xinfo = axis.info(pretty(c(a,b),6),c(a,b),sprintf("%s",names(input)[col]))
+    yinfo = axis.info(seq(0,1,0.1),c(0,1),"cumulative problem count ratio")
+    par(fig=c((col-1)/columns,col/columns,0,1), new=TRUE)
+    plot.data(xinfo, yinfo, data$name, data$values, data$classes)
+  }
+  dev.off()
 }
+
+
+# render("topkube_benchmark_problem_traits_distribution_0to1.pdf",c(0,1))
+render("/Users/llins/papers/xraytopk/tvcg/figs/topkube_benchmark_problem_traits_distribution_0to08.pdf",c(0,0.8))
+
+
 
